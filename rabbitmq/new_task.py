46b0c76c6eca18960a9feedb673d11597aaa1abf
Chris Nicel <chrisedebo@gmail.com>
2013-12-31 15:24:02 +0000

    rabbitmq things

---
+++ rabbitmq/new_task.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python2.7
+import pika
+import sys
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+  'localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message)
+
+print(" [x] Sent 'Hello World!'")
+
+connection.close
