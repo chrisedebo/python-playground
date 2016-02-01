a2105ecf60b3cd34914cefbcb452c7e800049d77
Chris Nicel <chris.nicel@15below.com>
2016-02-01 13:26:07 +0000

    Updated rabbit scripts

--- rabbitmq/helloworld-sender.py
+++ rabbitmq/helloworld-sender.py
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-#! /usr/bin/env python2.7
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-  'localhost'))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-
-print(" [x] Sent 'Hello World!'")
-
-connection.close
-=======
 #! /usr/bin/env python3.3
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-  'localhost'))
+  'btn-mq-dev'))
 
 channel = connection.channel()
 
@@ -34,4 +15,3 @@ channel.basic_publish(exchange='',
 print(" [x] Sent 'Hello World!'")
 
 connection.close
->>>>>>> 5373d7b508e85fad821eb6919378d6f8ef2ce868
--- rabbitmq/new_task.py
+++ rabbitmq/new_task.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-#! /usr/bin/env python2.7
-import pika
-import sys
-
-message = ' '.join(sys.argv[1:]) or "Hello World!"
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-  'localhost'))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-
-
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body=message)
-
-print(" [x] Sent 'Hello World!'")
-
-connection.close
-=======
 #! /usr/bin/env python2.7
 import pika
 import sys
@@ -49,4 +25,3 @@ broadcast(data_string)
 json_string = '''{ "firstName": "John","age": 25,"address": {"streetAddress": "21 2nd Street","city": "New York","state": "NY","postalCode": "10021"},"phoneNumber": [{"type": "home","number": "212 555-1234"},{"type": "fax","number": "646 555-4567"}]}'''
 
 broadcast(json_string)
->>>>>>> 5373d7b508e85fad821eb6919378d6f8ef2ce868
