69381d3ef9cf4e3d1912c33727bdc79df41c23dc
Chris Nicel <chris.nicel@15below.com>
2016-02-01 11:08:57 +0000

    updated things

---
+++ windows eventlogs.py
@@ -0,0 +1,27 @@
+import win32evtlog
+
+server = 'localhost' # name of the target computer to get event logs
+logtype = 'System'
+hand = win32evtlog.OpenEventLog(server,logtype)
+flags = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
+total = win32evtlog.GetNumberOfEventLogRecords(hand)
+
+print ('I made it to here!')
+
+events = win32evtlog.ReadEventLog(hand, flags,0)
+i = 0
+while True:
+    if events:
+        for event in events:
+            print ('Event Category:', event.EventCategory)
+            print ('Time Generated:', event.TimeGenerated)
+            print ('Source Name:', event.SourceName)
+            print ('Event ID:', event.EventID)
+            print ('Event Type:', event.EventType)
+            data = event.StringInserts
+            if data:
+                print ('Event Data:')
+                for msg in data:
+                    print (msg)
+            break
+
